a5b48425b01da5150ee630dc3c89e547ccd0fcb3
Stepan <Stepworm@yandex.ru>
2023-05-03 15:36:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exam;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("Hello world");
+            }
+        });
+    }
+}
