a9410c93d4f9ad62e10a4a6d1aeb6c9d42461faf
Stepan <Stepworm@yandex.ru>
2023-05-08 00:42:52 +0300

    runOnUI / nickname + time

--- app/src/main/java/com/example/exam/DataAdapter.java
+++ app/src/main/java/com/example/exam/DataAdapter.java
@@ -16,23 +16,26 @@ public class DataAdapter extends RecyclerView.Adapter<ViewHolder> {
     ArrayList<String> messages;
     ArrayList<String> nicknames;
     LayoutInflater inflater;
-
     public DataAdapter(Context context, ArrayList<String> messages, ArrayList<String> nicknames) {
         this.nicknames = nicknames;
         this.messages = messages;
         this.inflater = LayoutInflater.from(context);
     }
 
+    // метод создания нового ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // загружаем макет элемента списка и передаем его в конструктор ViewHolder
         View view = inflater.inflate(R.layout.item_message, parent, false);
 
         return new ViewHolder(view);
     }
 
+    // метод для привязки данных к элементам списка
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // если список сообщений или никнеймов пустой, то просто выходим из метода
         if(messages.size()*nicknames.size() == 0)
             return;
         String msg = messages.get(position);
@@ -42,6 +45,7 @@ public class DataAdapter extends RecyclerView.Adapter<ViewHolder> {
         holder.nickname.setText(nick);
     }
 
+    // метод для получения количества элементов списка
     @Override
     public int getItemCount() {
         return nicknames.size();
--- app/src/main/java/com/example/exam/MainActivity.java
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -46,17 +46,20 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> messages = new ArrayList<>();
     ArrayList<String> nicknames = new ArrayList<>();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        // Найти элементы пользовательского интерфейса по их идентификаторам
         mSendButton = findViewById(R.id.send_message_b);
         mEditTextMessage = findViewById(R.id.message_input);
         mEditNickname = findViewById(R.id.nickname_input);
 
         mTimeTextView = (TextView) findViewById(R.id.time);
+
+        // Установить таймер для обновления времени
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -72,33 +75,44 @@ public class MainActivity extends AppCompatActivity {
         mMessageRecycler = findViewById(R.id.message_recycler);
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
 
+        // Создать адаптер для списка сообщений и имен пользователей
         DataAdapter dataAdapter = new DataAdapter(this, messages, nicknames);
         mMessageRecycler.setAdapter(dataAdapter);
+
+        // Установить слушатель нажатия на кнопку "Отправить"
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String msg = mEditTextMessage.getText().toString();
                 String nick = mEditNickname.getText().toString();
                 String time = mTimeTextView.getText().toString();
-                nick = nick + " " + time;
+                nick = nick + " " + time; // Решил, что будет проще просто соединить никнейм и время
+                                          // вместо хранения дополнительного массива под время
+
+                // Проверяем, что сообщение и никнейм не пустые
                 if (msg.equals("") || nick.equals("")) {
                     Toast.makeText(MainActivity.this,R.string.empty_message_error, Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // Проверяем, что длина сообщения и никнейма не превышает максимально допустимую
                 if (msg.length() > MAX_MESSAGE_LENGTH || nick.length() > MAX_NICKNAME_LENGTH) {
                     Toast.makeText(MainActivity.this, R.string.length_message_error, Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // Отправляем сообщение и никнейм в базу данных
                 myRef.push().setValue(msg);
                 myRefNick.push().setValue(nick);
                 mEditTextMessage.setText("");
             }
         });
+
+        // Добавляем слушатель событий для базы данных Firebase Realtime Database для получения новых сообщений
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // Получаем новое сообщение и добавляем его в список сообщений
                 String msg = snapshot.getValue(String.class);
                 messages.add(msg);
             }
@@ -128,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String nick = snapshot.getValue(String.class);
                 nicknames.add(nick);
+                // Уведомляем адаптер о том, что данные изменились и прокручиваем список к последнему сообщению
                 dataAdapter.notifyDataSetChanged();
                 mMessageRecycler.smoothScrollToPosition(nicknames.size());
             }
@@ -155,22 +170,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Метод для получения текущего времени с сервера
+    // используя Retrofit для выполнения HTTP-запроса и получения JSON-ответа
     void fetchTime() {
+        // Создание объекта Retrofit и настройка базового URL-адреса для API
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://date.jsontest.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        // Создание объекта для вызова метода API
         JsontestAPI api = retrofit.create(JsontestAPI.class);
         Call<ServerTime> serverTimeCall = api.getServerDateTime();
+
+        // Выполнение запроса асинхронно и обработка ответа или ошибки через колбэки
         serverTimeCall.enqueue(new Callback<ServerTime>() {
             @Override
             public void onResponse(Call<ServerTime> call, Response<ServerTime> response) {
+                // Получение объекта ServerTime из тела ответа
                 ServerTime serverTime = response.body();
+                // Установка полученного времени в TextView
                 mTimeTextView.setText(serverTime.getTime());
             }
 
             @Override
             public void onFailure(Call<ServerTime> call, Throwable t) {
+                // Обработка ошибки при выполнении запроса
                 Toast.makeText(getApplicationContext(),
                         "Error" + t.toString(), Toast.LENGTH_SHORT).show();
             }
